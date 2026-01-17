7dbe94175ce4a5db89f6241957703076c218fec1
hyojin-10 <wowshally@gmail.com>
2026-01-17 17:14:01 +0900

    feat: Product 스키마 작성

---
+++ src/models/Product.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    tags: { type: [String], default: [] },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+export const Product = mongoose.model('Product', ProductSchema);
