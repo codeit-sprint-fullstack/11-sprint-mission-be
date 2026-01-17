49aa57f39c8cc45cdeedbd9f2567562bf0ddfc29
hyojin-10 <wowshally@gmail.com>
2026-01-17 19:02:19 +0900

    feat: CORS 미들웨어 적용

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import { connectDB } from './db/index.js';
 import { config } from './config/config.js';
 import { router } from './routes/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { cors } from './middlewares/cors.js';
 
 const app = express();
 await connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors);
 
 app.use('/api', router);
 
